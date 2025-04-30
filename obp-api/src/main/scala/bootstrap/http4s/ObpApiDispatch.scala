b65e5602e46816b0fcea011d4cc289cf9059415f
Hongwei <hongwei@tesobe.com>
2025-04-30 14:03:14 +0200

    feature: enhance error handling in OBP API proxy with detailed logging and JSON response

--- obp-api/src/main/scala/bootstrap/http4s/ObpApiDispatch.scala
+++ obp-api/src/main/scala/bootstrap/http4s/ObpApiDispatch.scala
@@ -2,9 +2,11 @@ package bootstrap.http4s
 
 import cats.effect.IO
 import cats.syntax.all._
+import code.api.util.ErrorMessages
 import code.util.Helper.MdcLoggable
 import org.http4s._
 import org.http4s.client.Client
+import org.http4s.headers.`Content-Type`
 
 class ObpApiDispatch(client: Client[IO], obpApiBaseUri: Uri) extends MdcLoggable{
 
@@ -27,18 +29,22 @@ class ObpApiDispatch(client: Client[IO], obpApiBaseUri: Uri) extends MdcLoggable
         body = req.body
       )
 
-      IO(logger.debug(s"[Proxy] ${req.method} ${req.uri} -> ${proxiedUri}")).flatMap { _ =>
+      IO(logger.debug(s"[Proxy] ${req.method} ${req.uri} -> ${proxiedUri}")) *>
         client.run(proxiedRequest).use { proxyResponse =>
-          IO(logger.debug(s"[Proxy] Response from ${proxiedUri}: ${proxyResponse.status}")).flatMap { _ =>
+          IO(logger.debug(s"[Proxy] Response from ${proxiedUri}: ${proxyResponse.status}")) *>
             Response[IO](
               status = proxyResponse.status,
               headers = proxyResponse.headers,
               body = proxyResponse.body
             ).pure[IO]
-          }
+        }.handleErrorWith { ex => 
+          IO(logger.error(s"[Proxy] Error proxying to OBP API: ${ex.getMessage}", ex)) *>
+            Response[IO](
+              status = Status.InternalServerError
+            ).withEntity(s"""{"message": "${ErrorMessages.UnknownError} Please check if OBP-API is dead, details:${ex.getMessage} "}""")
+              .withContentType(`Content-Type`(MediaType.application.json))
+              .pure[IO]
         }
-      }
   }
-
 
 }
