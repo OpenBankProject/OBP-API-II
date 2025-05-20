ae545d33a4726a060baa6a436588dbdd4226a904
Hongwei <hongwei@tesobe.com>
2025-05-20 11:54:54 +0200

    refactor/update AuthMiddleware and OAuthHandshakeHttp4s for improved error handling and response formatting

--- obp-api/src/main/scala/bootstrap/http4s/middleware/AuthMiddleware.scala
+++ obp-api/src/main/scala/bootstrap/http4s/middleware/AuthMiddleware.scala
@@ -7,7 +7,7 @@ import code.api.util.CallContext
 import code.api.util.ErrorMessages.{UnknownError, UserNotLoggedIn}
 import com.openbankproject.commons.model.User
 import org.http4s._
-import org.http4s.dsl.io._
+import org.http4s.headers.`Content-Type`
 
 object AuthMiddleware {
 
@@ -21,12 +21,20 @@ object AuthMiddleware {
           (userOpt, updatedCtx) = result
           response <- userOpt match {
             case Some(user) => handler(user, updatedCtx)
-            case None       => Forbidden(UserNotLoggedIn)
+            case None       =>
+              val errorJson = s"""{"code": 403, "message": "$UserNotLoggedIn"}"""
+              IO(Response[IO](status = Status.fromInt(403).getOrElse(Status.InternalServerError))
+                .withEntity(errorJson)
+                .withContentType(`Content-Type`(MediaType.application.json)))
           }
         } yield response
 
       case None =>
-        InternalServerError(s"$UnknownError CallContext missing")
+        val errorJson = s"""{"code": 500, "message": "$UnknownError CallContext missing"}"""
+        IO(Response[IO](status = Status.InternalServerError)
+          .withEntity(errorJson)
+          .withContentType(`Content-Type`(MediaType.application.json)))
+        
     }
   }
 }
--- obp-api/src/main/scala/code/api/OAuthHandshakeHttp4s.scala
+++ obp-api/src/main/scala/code/api/OAuthHandshakeHttp4s.scala
@@ -1,15 +1,19 @@
 package code.api
 
-import bootstrap.http4s.CallContextKeyProvider.callContextKey
-import cats.effect._
+import bootstrap.http4s.middleware.AuthMiddleware._
+import cats.effect.IO
+import cats.syntax.all._
 import code.api.oauth1a.OauthParams._
-import code.api.util.ErrorMessages.UnknownError
+import code.api.util.CallContext
 import code.token.Tokens
 import code.util.Helper.MdcLoggable
+import com.openbankproject.commons.model.User
 import net.liftweb.common._
+import org.http4s._
 import org.http4s.dsl.io._
 import org.http4s.headers.`Content-Type`
-import org.http4s._
+
+import scala.language.higherKinds
 
 /**
  * This object provides the API calls necessary to third party applications
@@ -21,92 +25,64 @@ object OAuthHandshakeHttp4s extends MdcLoggable {
   // Common prefix: /oauth
   val prefixPath = Root / "oauth"
 
-  // Route: POST /oauth/initiate
+
   val initiateRoute: HttpRoutes[IO] = HttpRoutes.of[IO] {
-    case req @ POST -> `prefixPath` / "initiate" =>
-      IO.defer {
-        logger.debug("Hello from oauth/initiate POST")
-
-        // Extract the CallContext from the request attributes
-        req.attributes.lookup(callContextKey) match {
-          case Some(callContext) =>
-            for {
-              // Extract the OAuth parameters from the header and test if the request is valid
-              validationResult <- IO.fromFuture(IO(OAuthHandshake.validatorFuture("requestToken", "POST", callContext)))
-              (httpCode, message, oAuthParameters) = validationResult
-              response <- if (httpCode == 200) {
-                // Generate the token and secret
-                val (token, secret) = OAuthHandshake.generateTokenAndSecret()
-                // Save the token that we have generated
-                val saved = OAuthHandshake.saveRequestToken(oAuthParameters, token, secret)
-                if (saved) {
-                  val responseMessage = s"oauth_token=$token&oauth_token_secret=$secret&oauth_callback_confirmed=true"
-                  Ok(responseMessage).map(_.withContentType(`Content-Type`(MediaType.application.`x-www-form-urlencoded`)))
-                } else {
-                  InternalServerError("Failed to save request token")
-                }
-              } else {
-                // Return an error response
-                Status.fromInt(httpCode).fold(
-                  _ => InternalServerError(message),
-                  status => IO.pure(Response[IO](status).withEntity(message))
-                )
-              }
-            } yield response
-          case None =>
-            InternalServerError(s"$UnknownError CallContext missing")
-        }
-      }
+    case req@POST -> `prefixPath` / "initiate" =>
+      securedEndpoint { (_: User, callContext: CallContext) =>
+        for {
+          validationResult <- IO.fromFuture(IO(OAuthHandshake.validatorFuture("requestToken", "POST", callContext)))
+          (httpCode, message, oAuthParameters) = validationResult
+          response <- if (httpCode == 200) {
+            val (token, secret) = OAuthHandshake.generateTokenAndSecret()
+            val saved = OAuthHandshake.saveRequestToken(oAuthParameters, token, secret)
+            if (saved) {
+              val responseMessage = s"oauth_token=$token&oauth_token_secret=$secret&oauth_callback_confirmed=true"
+              Ok(responseMessage).map(_.withContentType(`Content-Type`(MediaType.application.`x-www-form-urlencoded`)))
+            } else {
+              InternalServerError("Failed to save request token")
+            }
+          } else {
+            Status.fromInt(httpCode).fold(
+              _ => InternalServerError(message),
+              status => IO.pure(Response[IO](status).withEntity(message))
+            )
+          }
+        } yield response
+      }(req)
   }
 
-  // Route: POST /oauth/token
   val tokenRoute: HttpRoutes[IO] = HttpRoutes.of[IO] {
-    case req @ POST -> `prefixPath` / "token" =>
-      IO.defer {
-        // Extract the CallContext from the request attributes
-        req.attributes.lookup(callContextKey) match {
-          case Some(callContext) =>
-            for {
-              // Extract the OAuth parameters from the header and test if the request is valid
-              validationResult <- IO.fromFuture(IO(OAuthHandshake.validatorFuture("authorizationToken", "POST", callContext)))
-              (httpCode, message, oAuthParameters) = validationResult
-              response <- if (httpCode == 200) {
-                // Generate the token and secret
-                val (token, secret) = OAuthHandshake.generateTokenAndSecret()
-                // Save the token that we have generated
-                val saved = OAuthHandshake.saveAuthorizationToken(oAuthParameters, token, secret)
-                if (saved) {
-                  // Remove the request token so the application could not exchange it
-                  // again to get another access token
-                  Tokens.tokens.vend.getTokenByKey(oAuthParameters.get(TokenName).get) match {
-                    case Full(requestToken) => Tokens.tokens.vend.deleteToken(requestToken.id.get) match {
-                      case true =>
-                      case false => logger.warn("Request token: " + requestToken + " is not deleted. The application could exchange it again to get an other access token!")
-                    }
-                    case _ => logger.warn("Request token is not deleted due to absence in database!")
-                  }
-
-                  val responseMessage = s"oauth_token=$token&oauth_token_secret=$secret"
-                  Ok(responseMessage).map(_.withContentType(`Content-Type`(MediaType.application.`x-www-form-urlencoded`)))
-                } else {
-                  InternalServerError("Failed to save authorization token")
+    case req@POST -> `prefixPath` / "token" =>
+      securedEndpoint { (_: User, callContext: CallContext) =>
+        for {
+          validationResult <- IO.fromFuture(IO(OAuthHandshake.validatorFuture("authorizationToken", "POST", callContext)))
+          (httpCode, message, oAuthParameters) = validationResult
+          response <- if (httpCode == 200) {
+            val (token, secret) = OAuthHandshake.generateTokenAndSecret()
+            val saved = OAuthHandshake.saveAuthorizationToken(oAuthParameters, token, secret)
+            if (saved) {
+              Tokens.tokens.vend.getTokenByKey(oAuthParameters.get(TokenName).get) match {
+                case Full(requestToken) => Tokens.tokens.vend.deleteToken(requestToken.id.get) match {
+                  case true =>
+                  case false => logger.warn("Request token: " + requestToken + " is not deleted. The application could exchange it again to get an other access token!")
                 }
-              } else {
-                // Return an error response
-                Status.fromInt(httpCode).fold(
-                  _ => InternalServerError(message),
-                  status => IO.pure(Response[IO](status).withEntity(message))
-                )
+                case _ => logger.warn("Request token is not deleted due to absence in database!")
               }
-            } yield response
-          case None =>
-            InternalServerError(s"$UnknownError CallContext missing")
-        }
-      }
+              val responseMessage = s"oauth_token=$token&oauth_token_secret=$secret"
+              Ok(responseMessage).map(_.withContentType(`Content-Type`(MediaType.application.`x-www-form-urlencoded`)))
+            } else {
+              InternalServerError("Failed to save authorization token")
+            }
+          } else {
+            Status.fromInt(httpCode).fold(
+              _ => InternalServerError(message),
+              status => IO.pure(Response[IO](status).withEntity(message))
+            )
+          }
+        } yield response
+      }(req)
   }
-
   // All routes combined
   val allRoutes: HttpRoutes[IO] =
-    initiateRoute 
-  //<+> tokenRoute
+    initiateRoute <+> tokenRoute
 }
